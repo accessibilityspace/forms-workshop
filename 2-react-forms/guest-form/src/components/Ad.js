08b94615f8f84a7763a15aa73c132664af8a7e37
Jay Kid <cyph3rfox@gmail.com>
2019-02-27 16:26:45 +0100

    feat: make ad invisible for screen reader users <3
    Since we usually don't have a way of describing ads (they arrive randomly from a server far far away that only cares about impressions and money) we just hide it so the user doesn't have to suffer a very long string of characters that conform the image URL being read out loud.

--- 2-react-forms/guest-form/src/components/Ad.js
+++ 2-react-forms/guest-form/src/components/Ad.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Ad extends React.Component {
   render () {
-    return (<img className="advertisement" src="https://c1.staticflickr.com/1/198/484322226_c7e260fb90_b.jpg"></img>)
+    return (<img aria-hidden="true" className="advertisement" src="https://c1.staticflickr.com/1/198/484322226_c7e260fb90_b.jpg"></img>)
   }
 }
 
