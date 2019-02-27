7c72e60f546c12436c72ba8766d078bf12304d8f
Jay Kid <cyph3rfox@gmail.com>
2019-02-27 16:24:16 +0100

    feat: add some beautiful advertisement <3
    Props to edkohler for making such awesome picture Creative Commons :)
    
    You can find it here: https://www.flickr.com/photos/edkohler/484322226

--- 2-react-forms/guest-form/src/App.js
+++ 2-react-forms/guest-form/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import Ad from './components/Ad';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <main>
         <h1>The Magical Inn registration form</h1>
+        <Ad></Ad>
         <Form/>
       </main>
     );
---
+++ 2-react-forms/guest-form/src/components/Ad.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Ad extends React.Component {
+  render () {
+    return (<img className="advertisement" src="https://c1.staticflickr.com/1/198/484322226_c7e260fb90_b.jpg"></img>)
+  }
+}
+
+export default Ad;
